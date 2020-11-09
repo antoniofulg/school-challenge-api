71a7764b00b1c2d2e8571cd0165b733af7158b08
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-11-09 17:46:52 -0300

    fix: added profile field

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm'
+import Profile from './Profile'
 
 @Entity('teachers')
 export default class Teacher {
@@ -7,4 +8,7 @@ export default class Teacher {
 
   @Column()
   name: string
+
+  @OneToOne(() => Profile, (profile) => profile.teacher)
+  profile: Profile
 }
